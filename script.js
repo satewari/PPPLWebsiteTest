49821d52cb2fc2a551cd3795f9c875bef69f6d95
shtewari <71026086+shtewari@users.noreply.github.com>
2021-11-23 16:54:52 -0500

    tab reduction
    Reduced the amount of tabs open simultaneously by closing the last tab opened

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 /** Counter variable outside of the function call; 
 this variable will essentially serve as a counter 
 for how many websites are left. **/
-var counter = 0; 
+var counter = 0;
+var myTab;
 function openTab() {
-    const link = document.createElement('a'); // Create an empty link.
+    if (myTab) { //Closes the last tab, if that last tab existed (i.e. on first loop)
+        myTab.close();
+    }
     /** Array of URLs you want. Can be any length. **/
     const arry = ['https://errorsea.com', 'https://google.com', 'https://stackoverflow.com'] 
     if(counter < 3) { //As long as the list is not exhausted, create, go to, and close the links from the array of URLs.
-        link.href = arry[counter];
-        link.target = '_blank';
-        document.body.appendChild(link);
-        link.click();
-        link.remove();
+        myTab = window.open(arry[counter], "_blank");
     }
     counter++; //Go to the next URL and wait for next function call.
 }
